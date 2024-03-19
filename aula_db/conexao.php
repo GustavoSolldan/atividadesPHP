e51ebd440b8385e53ac112ad1452ba7ad39e5249
Gustavo Soldan <engs-gustavosoldan@camporeal.edu.br>
2024-03-19 20:09:51 -0300

    feat(front-select): cria select na tela tarefas pedentes

--- aula_db/conexao.php
+++ aula_db/conexao.php
@@ -1,25 +1,28 @@
-<?php 
+<?php
 
-    class Conexao{
-        private $host = 'localhost';
-        private $dbname = 'php_com_pdo';
-        private $user = 'root';
-        private $password = '';
+class Conexao {
 
-        public function conectar() {
-            try{
-                $conexao = new PDO(
-                    "mysql:host=$this->host;dbname=$this->dbname",
-                    "$this->user",
-                    "$this->password"
-                );
-                
-                return $conexao;
-            } catch (PDOException $e) {
-                echo '<p>'.$e->getMessage().'</p>';
-            }
+	private $host = 'localhost';
+	private $dbname = 'php_com_pdo';
+	private $user = 'root';
+	private $pass = '';
 
-            
-        }
-    }
+	public function conectar() {
+		try {
 
+			$conexao = new PDO(
+				"mysql:host=$this->host;dbname=$this->dbname",
+				"$this->user",
+				"$this->pass"				
+			);
+
+			return $conexao;
+
+
+		} catch (PDOException $e) {
+			echo '<p>'.$e->getMessage().'</p>';
+		}
+	}
+}
+
+?>
--- aula_db/tarefa_controller.php
+++ aula_db/tarefa_controller.php
@@ -1,58 +1,98 @@
 <?php
 
-    require "./tarefa.model.php";
-    require "./tarefa.service.php";
-    require "./conexao.php";
+	require "tarefa.model.php";
+	require "tarefa.service.php";
+	require "conexao.php";
 
-    $acao = isset($_GET['acao']) ? $_GET['acao'] : $acao;
+	$acao = isset($_GET['acao']) ? $_GET['acao'] : $acao;
 
-    if ($acao == 'inserir') {
-        $tarefa = new Tarefa();
-        $tarefa->__set('tarefa', $_POST['tarefa']);
+	if($acao == 'inserir' ) {
+		$tarefa = new Tarefa();
+		$tarefa->__set('tarefa', $_POST['tarefa']);
 
-        $conexao = new Conexao();
+		$conexao = new Conexao();
 
-        $tarefaService = new TarefaService($conexao, $tarefa);
-        $tarefaService->inserir();
+		$tarefaService = new TarefaService($conexao, $tarefa);
+		$tarefaService->inserir();
 
-        header('location: (próximos passos)');
-    } else if ($acao == 'recuperar') {
-        $tarefa = new Tarefa();
-        $conexao = new Conexao();
+		header('Location: nova_tarefa.php?inclusao=1');
+	
+	} else if($acao == 'recuperar') {
+		
+		$tarefa = new Tarefa();
+		$conexao = new Conexao();
 
-        $tarefaService = new TarefaService($conexao, $tarefa);
-        $tarefas = $tarefaService->recuperar();
-    }
-    else if ($acao == 'atualizar') {
-        $tarefa = new Tarefa();
-        $tarefa->__set('id', $_POST['id']);
-        $tarefa->__set('tarefa', $_POST['tarefa']);
-        $conexao = new Conexao();
+		$tarefaService = new TarefaService($conexao, $tarefa);
+		$tarefas = $tarefaService->recuperar();
+	
+	} else if($acao == 'atualizar') {
 
-        $tarefaService = new TarefaService($conexao, $tarefa);
-        $tarefaService->atualizar();
-    }
-    else if ($acao == 'remover') {
-        $tarefa = new Tarefa();
-        $tarefa->__set('id', $_GET['id']);
-        $conexao = new Conexao();
+		$tarefa = new Tarefa();
+		$tarefa->__set('id', $_POST['id'])
+			->__set('tarefa', $_POST['tarefa']);
 
-        $tarefaService = new TarefaService($conexao, $tarefa);
-        $tarefaService->remover();
-    }
-    else if ($acao == 'recuperarTarefasPendentes') {
-        $tarefa = new Tarefa();
-        $tarefa->__set('id_status', 0);
-        $conexao = new Conexao();
+		$conexao = new Conexao();
 
-        $tarefaService = new TarefaService($conexao, $tarefa);
-        $tarefas = $tarefaService->recuperarTarefasPendentes();
-    }
-    else if ($acao == 'marcarRealizada') {
-        $tarefa = new Tarefa();
-        $tarefa->__set('id', $_GET['id']->__set('id_status', 1));
-        $conexao = new Conexao();
+		$tarefaService = new TarefaService($conexao, $tarefa);
+		if($tarefaService->atualizar()) {
+			
+			if( isset($_GET['pag']) && $_GET['pag'] == 'index') {
+				header('location: index.php');	
+			} else {
+				header('location: todas_tarefas.php');
+			}
+		}
 
-        $tarefaService = new TarefaService($conexao, $tarefa);
-        $tarefaService->marcarRealizada();
-    }
+
+	} else if($acao == 'remover') {
+
+		$tarefa = new Tarefa();
+		$tarefa->__set('id', $_GET['id']);
+
+		$conexao = new Conexao();
+
+		$tarefaService = new TarefaService($conexao, $tarefa);
+		$tarefaService->remover();
+
+		if( isset($_GET['pag']) && $_GET['pag'] == 'index') {
+			header('location: index.php');	
+		} else {
+			header('location: todas_tarefas.php');
+		}
+	
+	} else if($acao == 'marcarRealizada') {
+
+		$tarefa = new Tarefa();
+		$tarefa->__set('id', $_GET['id'])->__set('id_status', 2);
+
+		$conexao = new Conexao();
+
+		$tarefaService = new TarefaService($conexao, $tarefa);
+		$tarefaService->marcarRealizada();
+
+		if( isset($_GET['pag']) && $_GET['pag'] == 'index') {
+			header('location: index.php');	
+		} else {
+			header('location: todas_tarefas.php');
+		}
+	
+	} else if($acao == 'recuperarTarefasPendentes') {
+		$tarefa = new Tarefa();
+		$tarefa->__set('id_status', 1);
+		
+		$conexao = new Conexao();
+
+		$tarefaService = new TarefaService($conexao, $tarefa);
+		$tarefas = $tarefaService->recuperarTarefasPendentes();
+	}
+	else if($acao == 'ordenar') {
+		$criterio = isset($_GET['criterio']) ? $_GET['criterio'] : 'data_criacao';
+		$tarefa = new Tarefa();
+		$conexao = new Conexao();
+	
+		$tarefaService = new TarefaService($conexao, $tarefa);
+		$tarefas = $tarefaService->ordenarTarefas($criterio);
+	}
+
+
+?>
